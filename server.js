b27df9dcdd38e427df8f92e91450f76171977131
Woody Gelius <gelius24@hotmail.com>
2021-08-05 07:10:01 -0400

    ...

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ console.log("client id",  process.env.CLIENT_ID)
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function (req, res) {
+  app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 };

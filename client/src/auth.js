b50edd1c962e137e6f1444623c85fb75abf52143
Woody Gelius <gelius24@hotmail.com>
2021-08-05 04:14:59 -0400

    ,,,

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 // import { url } from './Login'
-const port = process.env.PORT || 3001;
-console.log("redirect url >", process.env.REDIRECT_URI);
-console.log("process.env.PORT :::", process.env.PORT);
-console.log("le ID client est >>>", process.env.CLIENT_ID);
+const port = process.env.REACT_APP_PORT || 3001;
+console.log("redirect url >", process.env.REACT_APP_REDIRECT_URI);
+console.log("process.env.PORT :::", process.env.REACT_APP_PORT);
+console.log("le ID client est >>>", process.env.REACT_APP_SECRET_ID);
 
 let url = "";
 if (window.location.origin === "http://localhost:3000") {
